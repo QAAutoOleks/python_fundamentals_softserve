5a7e118ff9aae7d6a07e0ef312c181d52be13313
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-03 20:15:06 +0300

    modified:   mutable_or_immutable_testing_2.py

--- mutable_or_immutable_testing_2.py
+++ mutable_or_immutable_testing_2.py
@@ -1,8 +1,17 @@
-a = 3
-b = a
-c = a
-print(id(b))
-print(id(c))
+# a = 3
+# b = a
+# c = a
+# print(id(b))
+# print(id(c))
 
-a = "New name"
+# a = "New name"
+# print(id(b))
+
+a = True
+print(id(a))
+
+b = False
 print(id(b))
+
+b = True
+print(f"id(a) = {id(a)}", f"id(b) = {id(b)}")
