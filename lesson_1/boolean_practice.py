895bf48221c88fcf9e0a29a8087f4b7afa43acf9
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-03 20:00:07 +0300

    modified:   lesson_1/boolean_practice.py new file:   lesson_1/input_practice.py

--- lesson_1/boolean_practice.py
+++ lesson_1/boolean_practice.py
@@ -1,3 +1,3 @@
 print(bool(0))
 print(bool(3.14159))
-print(bool(1.0+1j))
+# print(bool(1.0+1j))
---
+++ lesson_1/input_practice.py
@@ -0,0 +1,9 @@
+name = input("What is your name?\n")
+age = input("How old are you?\n")
+city = input("Where do you live?\n")
+
+print("---------------")
+
+print("Hello", name, "!")
+print("Your age is", age, ".")
+print("You live in", city, ".")
