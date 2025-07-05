edf41bb30c70407cba568256c0b2eca5c561b507
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-05 22:10:09 +0300

    new file:   lesson_2/practice_2.py new file:   lesson_2/practice_3.py new file:   lesson_2/practice_4.py

---
+++ lesson_2/practice_3.py
@@ -0,0 +1,4 @@
+minutes = int(input())
+convert = minutes * 60
+
+print(convert)
---
+++ lesson_2/practice_4.py
@@ -0,0 +1,9 @@
+R = int(input())
+r = int(input())
+
+PI = 3.14
+
+full_result = 4 * PI / 3 * (R**3 - r**3)
+result = round(full_result, 2)
+
+print(result)
