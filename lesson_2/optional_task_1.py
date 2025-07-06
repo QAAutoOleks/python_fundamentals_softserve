21e7fbe23651b56e08156966576820bd3c4880e7
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-06 18:28:51 +0300

    modified:   lesson_2/optional_task_1.py

--- lesson_2/optional_task_1.py
+++ lesson_2/optional_task_1.py
@@ -1,4 +1,4 @@
-number = 1234
+number = 5271
 
 number_to_str = str(number)
 product = (int(number_to_str[0])  
@@ -9,4 +9,9 @@ product = (int(number_to_str[0])
 reversed_string = number_to_str[::-1]
 reversed_number = int(reversed_string)
 
-sorted_number = 0
+sorted_list = sorted(number_to_str)
+sorted_str = (sorted_list[0]
+            + sorted_list[1]
+            + sorted_list[2]
+            + sorted_list[3]) 
+sorted_number = int(sorted_str)
