3fcf35c8c1a3a8b46e2791c7a19aa5c6092979ec
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-06 18:13:19 +0300

    modified:   lesson_2/optional_task_1.py

--- lesson_2/optional_task_1.py
+++ lesson_2/optional_task_1.py
@@ -1,12 +1,12 @@
-number = 1111
+number = 1234
 
 number_to_str = str(number)
 product = (int(number_to_str[0])  
         *  int(number_to_str[1]) 
         *  int(number_to_str[2]) 
         *  int(number_to_str[3]))
-print(product)
 
-reversed_number = 0
+reversed_string = number_to_str[::-1]
+reversed_number = int(reversed_string)
 
 sorted_number = 0
