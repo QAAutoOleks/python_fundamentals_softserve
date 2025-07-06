944aa3dd5c7143060686811d69decdd6ec137efd
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-06 19:16:44 +0300

    modified:   lesson_2/optional_task_2.py

--- lesson_2/optional_task_2.py
+++ lesson_2/optional_task_2.py
@@ -2,13 +2,12 @@ import this
 import codecs
 
 python_philosophy = codecs.decode(this.s, 'rot_13')
-print(python_philosophy)
 
-better_count = ""
+better_count = python_philosophy.count('better')
 
-never_count = ""
+never_count = python_philosophy.count('never')
 
-is_count = ""
+is_count = python_philosophy.count('is')
 
 zen_upper = ""
 
