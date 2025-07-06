f357acbdf3820f527b97550516b59d76229d3065
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-06 18:09:38 +0300

    new file:   lesson_2/optional_task_1.py new file:   lesson_2/practice_5.py

---
+++ lesson_2/optional_task_1.py
@@ -0,0 +1,12 @@
+number = 1111
+
+number_to_str = str(number)
+product = (int(number_to_str[0])  
+        *  int(number_to_str[1]) 
+        *  int(number_to_str[2]) 
+        *  int(number_to_str[3]))
+print(product)
+
+reversed_number = 0
+
+sorted_number = 0
---
+++ lesson_2/practice_5.py
@@ -0,0 +1,7 @@
+base = int(input())
+height = int(input())
+
+tri_area = 1 / 2 * base * height
+area = round(tri_area, 2)
+
+print(area)
