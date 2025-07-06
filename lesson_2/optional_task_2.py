6d20c569f49bcb057cbb01d28eee5cdf70e254b0
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-06 19:51:08 +0300

    modified:   lesson_2/optional_task_2.py

--- lesson_2/optional_task_2.py
+++ lesson_2/optional_task_2.py
@@ -1,7 +1,17 @@
-import this
+import importlib
+import sys
+import io
+
+buffer = io.StringIO()
+sys.stdout = buffer
+
+this = importlib.import_module("this")
+sys.stdout = sys.__stdout__
+
 import codecs
 
-python_philosophy = codecs.decode(this.s, 'rot_13')
+python_philosophy_full = codecs.decode(this.s, 'rot_13')
+python_philosophy = python_philosophy_full[34::]
 
 better_count = python_philosophy.count('better')
 
@@ -9,6 +19,7 @@ never_count = python_philosophy.count('never')
 
 is_count = python_philosophy.count('is')
 
-zen_upper = ""
+zen_upper = python_philosophy.upper()
 
-zen_replace = ""
+zen_replace = python_philosophy.replace('i', '&')
+print(zen_replace)
