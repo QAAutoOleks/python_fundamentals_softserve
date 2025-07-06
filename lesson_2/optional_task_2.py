c2bb1f90787eb8a94040ddd1c833750d44493ccb
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-06 19:06:10 +0300

    new file:   lesson_2/optional_task_2.py

---
+++ lesson_2/optional_task_2.py
@@ -0,0 +1,15 @@
+import this
+import codecs
+
+python_philosophy = codecs.decode(this.s, 'rot_13')
+print(python_philosophy)
+
+better_count = ""
+
+never_count = ""
+
+is_count = ""
+
+zen_upper = ""
+
+zen_replace = ""
