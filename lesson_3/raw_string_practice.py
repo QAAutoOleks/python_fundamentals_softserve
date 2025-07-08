ce56f37e45a97db9f62ec0e3c2fd5c247bbcaada
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-08 20:41:59 +0300

    modified:   lesson_3/raw_string_practice.py new file:   lesson_3/slicing_practice.py

--- lesson_3/raw_string_practice.py
+++ lesson_3/raw_string_practice.py
@@ -1,2 +1,5 @@
 # \ before " without a space - ERROR
 rstr = r"It's a division - \ "
+
+# next example is OK
+text = r"C:\new_folder\test.txt"
---
+++ lesson_3/slicing_practice.py
@@ -0,0 +1,2 @@
+s = '0123456789'
+print(s[8:-5]) # None output
