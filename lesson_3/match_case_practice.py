2ef66803d2a610c7c36e3ac6984c67df1b81bddf
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-14 18:34:53 +0300

    modified:   lesson_3/match_case_practice.py

--- lesson_3/match_case_practice.py
+++ lesson_3/match_case_practice.py
@@ -1,9 +1,10 @@
 some_input = ['Text', 1, 2]
 
 match some_input:
-    # case 'Tex', 1, 2:
-    case 'Text', *num:
-    # case 'Text', 1, 2:
+    # case 'Tex', 1, 2: - Error
+    # case 'Text', *num: - Ok
+    # case 'Text', 1, 2: - Ok
+    case text, 1, 2: # Ok
         print('Ok')
     case _:
         print('Error')
