67a0062a5f4c63aa4bba3f4d8c04fc171dd28e21
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-14 19:47:02 +0300

    modified:   lesson_3/even_odd_100.py

--- lesson_3/even_odd_100.py
+++ lesson_3/even_odd_100.py
@@ -1,2 +1,8 @@
+# for i in range(0, 101, 2):
+#     print(f'Even number: {i}')
+
+list_even_numbers = []
 for i in range(0, 101, 2):
-    print(f'Even number: {i}')
+    list_even_numbers.append(i)
+
+print(list_even_numbers)
