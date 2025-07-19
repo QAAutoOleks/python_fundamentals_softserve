17d65c66c2359d43767f3d78f966ba805c2aad7b
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-19 23:10:11 +0300

    new file:   lesson_5_collections/practical_task_5.py

---
+++ lesson_5_collections/practical_task_5.py
@@ -0,0 +1,14 @@
+lst = input()
+num = int(input())
+
+int_list = [int(i.strip()) for i in lst.split(',')]
+int_list.sort()
+
+counter = 0
+for element in int_list:
+    if element < num:
+        counter += 1
+    else:
+        result = round(100 * (len(int_list) - counter) / len(int_list), 1)
+
+print(result)
