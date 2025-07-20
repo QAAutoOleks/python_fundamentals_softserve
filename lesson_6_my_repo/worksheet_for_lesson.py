8aa3f11b057c671a34fed60170282dd3a554de08
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-07-20 17:39:04 +0300

    modified:   lesson_6_my_repo/worksheet_for_lesson.py

--- lesson_6_my_repo/worksheet_for_lesson.py
+++ lesson_6_my_repo/worksheet_for_lesson.py
@@ -1,4 +1,6 @@
-list_of_integers = [int(input()) for i in range(3)]
+quantity_of_numbers = 3
+print(f"Enter {quantity_of_numbers} numbers..")
+list_of_integers = [int(input(f"Enter {i+1} number: ")) for i in range(3)]
 
 print(f"Max value is: {max(list_of_integers)}")
 print(f"Min value is: {min(list_of_integers)}")
