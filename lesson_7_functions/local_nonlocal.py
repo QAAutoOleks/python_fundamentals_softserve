d35923b4c6391987b68a7a8979e5bbb8083a84a9
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-07-24 22:28:03 +0300

    modified:   lesson_7_functions/local_nonlocal.py

--- lesson_7_functions/local_nonlocal.py
+++ lesson_7_functions/local_nonlocal.py
@@ -9,7 +9,15 @@ def outer_func():
         print(f"Inner x = {x}")
     
     inner_func()
-    print(f"Outer x = {x}")
+    print(f"Outer x = {x}\n")
+
+    def inner_nonlocal():
+        nonlocal x
+        x = 999
+        print(f"Inner_nonlocal x = {x}")
+    
+    inner_nonlocal()
+    print(f"Updated outer x = {x}")
 
 outer_func()
 print(f"Global x = {x}")
