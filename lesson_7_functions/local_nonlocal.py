74abe26f5bf445acf757d29056d8faa39adb2995
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-07-25 16:38:32 +0300

    modified:   lesson_7_functions/local_nonlocal.py

--- lesson_7_functions/local_nonlocal.py
+++ lesson_7_functions/local_nonlocal.py
@@ -1,11 +1,14 @@
 x = 100
+print(f"Id x1: {id(x)}")
 
 def outer_func():
     x = 1000
+    print(f"Id x2: {id(x)}")
 
     def inner_func():
         global x
         x = 10000
+        print(f"Id x3: {id(x)}")
         print(f"Inner x = {x}")
     
     inner_func()
@@ -14,6 +17,8 @@ def outer_func():
     def inner_nonlocal():
         nonlocal x
         x = 999
+        print(f"Id x4: {id(x)}")
+        
         print(f"Inner_nonlocal x = {x}")
     
     inner_nonlocal()
@@ -21,3 +26,4 @@ def outer_func():
 
 outer_func()
 print(f"Global x = {x}")
+print(f"Id x1: {id(x)}")
