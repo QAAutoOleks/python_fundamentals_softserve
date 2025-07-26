f27b4df6e3f9db907554351ead46db883a7b45c1
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-07-26 11:53:24 +0300

    modified:   lesson_7_functions/factorial_ordinary_way.py

--- lesson_7_functions/factorial_ordinary_way.py
+++ lesson_7_functions/factorial_ordinary_way.py
@@ -8,7 +8,7 @@ match given_number:
     case a if given_number < 0:
         print("Factorial of negative number doesn't exist")
     case _:
-        for i in range(1, given_number+1, 1):
+        for i in range(1, given_number+1):
             factorial_output *= i
         print("Factorial of '{}' =".format(given_number), factorial_output)
         
