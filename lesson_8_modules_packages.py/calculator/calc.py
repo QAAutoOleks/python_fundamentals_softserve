5681d3d6c403b8dd0f4a38af726b98958ef6f7f7
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-07-29 10:50:51 +0300

    new file:   lesson_8_modules_packages.py/calculator/__pycache__/module_with_functions.cpython-312.pyc modified:   lesson_8_modules_packages.py/calculator/calc.py modified:   lesson_8_modules_packages.py/calculator/module_with_functions.py

--- lesson_8_modules_packages.py/calculator/calc.py
+++ lesson_8_modules_packages.py/calculator/calc.py
@@ -1,3 +1,17 @@
 import module_with_functions
 
+def main():
+    print("Choose operation:")
+    print("1) Addition")
+    print("2) Subtraction")
+    print("3) Multiplication")
+    print("4) Division")
 
+main()
+chosen_option = int(input())
+number_1 = int(input("Enter number 1: "))
+number_2 = int(input("Enter number 2: "))
+
+match chosen_option:
+    case 1:
+        module_with_functions.addition(number_1, number_2)
--- lesson_8_modules_packages.py/calculator/module_with_functions.py
+++ lesson_8_modules_packages.py/calculator/module_with_functions.py
@@ -1,5 +1,5 @@
 def addition(number_1, number_2):
-    return number_1 + number_2
+    return print("The sum of Numer 1 and Number 2 is equal: {}".format(number_1 + number_2))
 
 def subtraction(number_1, number_2):    
     return number_1 - number_2
