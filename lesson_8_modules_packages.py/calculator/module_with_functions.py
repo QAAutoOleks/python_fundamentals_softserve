bdd8471877b5d87ced0fb5ebd02184411900f948
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-07-29 11:43:17 +0300

    modified:   lesson_8_modules_packages.py/calculator/module_with_functions.py modified:   lesson_8_modules_packages.py/calculator/__pycache__/module_with_functions.cpython-312.pyc

--- lesson_8_modules_packages.py/calculator/module_with_functions.py
+++ lesson_8_modules_packages.py/calculator/module_with_functions.py
@@ -4,17 +4,21 @@ def input_numbers_getting():
     return [number_1, number_2]
 
 def addition():
+    required_numbers = input_numbers_getting()
     return print("The sum of Number 1 and Number 2 is equal: {}"
-                 .format(input_numbers_getting()[0] + input_numbers_getting()[1]))
+                 .format(required_numbers[0] + required_numbers[1]))
 
 def subtraction():    
+    required_numbers = input_numbers_getting()
     return print("The difference between Number 1 and Number 2 is equal: {}"
-                 .format(input_numbers_getting()[0] - input_numbers_getting()[1]))
+                 .format(required_numbers[0] - required_numbers[1]))
 
 def multiplication():
+    required_numbers = input_numbers_getting()
     return print("The product of the multiplication Number 1 on Number 2 is equal: {}"
-                 .format(input_numbers_getting()[0] * input_numbers_getting()[1]))
+                 .format(required_numbers[0] * required_numbers[1]))
 
 def division():
+    required_numbers = input_numbers_getting()
     return print("The quotient of the division Number 1 by Number 2 is equal: {}"
-                 .format(input_numbers_getting()[0] / input_numbers_getting()[1]))
+                 .format(required_numbers[0] / required_numbers[1]))
