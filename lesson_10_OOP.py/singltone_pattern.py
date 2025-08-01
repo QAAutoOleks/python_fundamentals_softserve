fe252a72078552344ac1f5e628626b35843b1417
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-01 12:41:16 +0300

    modified:   lesson_10_OOP.py/singltone_pattern.py

--- lesson_10_OOP.py/singltone_pattern.py
+++ lesson_10_OOP.py/singltone_pattern.py
@@ -3,8 +3,9 @@ class Singltone():
     obj = None
 
     def __new__(cls, *args, **kwargs):
-        # If it doesn't exist yet, then
-        # call __new__ of the parent class
-        cls.obj = object.__new__(cls, *args, **kwargs)
+        if cls.obj is None:
+            # If it doesn't exist yet, then
+            # call __new__ of the parent class
+            cls.obj = object.__new__(cls, *args, **kwargs)
 
         return cls.obj # returns Singltone
