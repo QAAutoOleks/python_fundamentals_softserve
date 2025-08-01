ded18fe3f884aeb1f81f3c14128b7fc50bee810f
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-01 12:46:45 +0300

    modified:   lesson_10_OOP.py/singltone_pattern.py

--- lesson_10_OOP.py/singltone_pattern.py
+++ lesson_10_OOP.py/singltone_pattern.py
@@ -9,3 +9,14 @@ class Singltone():
             cls.obj = object.__new__(cls, *args, **kwargs)
 
         return cls.obj # returns Singltone
+    
+
+object1 = Singltone()
+print(object1)
+
+object1.attribute = 10
+
+object2 = Singltone()
+print(object1)
+print(object2)
+print('Attribute of object2:', object2.attribute)
