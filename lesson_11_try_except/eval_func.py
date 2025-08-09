3f39429f679cc8d66edf4a6d3b71dd66879d8932
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-09 08:18:22 +0300

    new file:   lesson_11_try_except/eval_func.py new file:   lesson_11_try_except/practical_task_1.py

---
+++ lesson_11_try_except/eval_func.py
@@ -0,0 +1,5 @@
+# x = 'print(55)'
+# eval(x)
+
+num1, num2 = eval(input("Enter two numbers, separated by comma: "))
+print(num1 / num2)
---
+++ lesson_11_try_except/practical_task_1.py
@@ -0,0 +1,12 @@
+def valid_number(input_number):
+    if not isinstance(input_number, int) or input_number == 0:
+        return False
+    else:
+        return True
+    
+def check_odd_even(input_number):
+    
+    
+    # "Entered number is even"
+    # "Entered number is odd
+    # "You entered not a number."
