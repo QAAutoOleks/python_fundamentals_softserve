3a29706c0d716ab4f899a85e267ff7804d239b4c
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-09 11:41:37 +0300

    modified:   lesson_11_try_except/practical_task_2.py

--- lesson_11_try_except/practical_task_2.py
+++ lesson_11_try_except/practical_task_2.py
@@ -3,20 +3,19 @@ class MyError(Exception):
         self.data = data
 
     def __str__(self):
-        return repr(self.data)
-    
-def valid_data(number):
-    if not isinstance(number, (float, int)):
-        return 'Error type: ValueError!'
-    if number > 0:
-        return f'You input positive number: {number}'
-    elif number < 0:
-        raise MyError(f'You input negative number: {number}. Try again.')
+        return self.data
+
 
 def check_positive(number):
     try:
-        return valid_data(number)
+        number = float(number)
+        if number > 0:
+            return f'You input positive number: {number}'
+        else:
+            raise MyError(f'You input negative number: {number}. Try again.')
     except MyError as m:
         return m
+    except:
+        return 'Error type: ValueError!'
     
-print(check_positive(8.9))
+print(check_positive(-9))
