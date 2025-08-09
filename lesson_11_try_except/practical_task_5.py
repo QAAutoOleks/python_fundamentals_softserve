58889ebd01a11f4b6a85ac2f0d1b4c768ce6c8e3
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-09 19:38:23 +0300

    new file:   lesson_11_try_except/practical_task_5.py

---
+++ lesson_11_try_except/practical_task_5.py
@@ -0,0 +1,18 @@
+import re
+from random import choices
+from string import ascii_letters, digits
+
+
+def check(login):
+    if not re.match(r"^(?=.*(?:employee|admin))(?=.*\d).*$", login, re.IGNORECASE):
+        raise ValueError(f"incorrect login '{login}'")
+    return True
+    
+letters_and_digits = choices(ascii_letters, k=5)
+letters_and_digits.extend(choices(digits, k=5))
+incorect_login = ''.join(letters_and_digits)
+try:
+    if check(incorect_login):
+        print("checked successfully")
+except ValueError as e:
+    print(str(e) == f"incorrect login '{incorect_login}'")
