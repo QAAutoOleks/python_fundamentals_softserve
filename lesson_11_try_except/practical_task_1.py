28affad84affdf1dd89bf72e01324cb59cf3ee08
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-09 08:52:59 +0300

    modified:   lesson_11_try_except/practical_task_1.py

--- lesson_11_try_except/practical_task_1.py
+++ lesson_11_try_except/practical_task_1.py
@@ -1,13 +1,10 @@
 def valid_number(input_number):
     if not isinstance(input_number, int):
         raise TypeError('Number must be integer')
-    elif input_number == 0:
-        raise ValueError('Number can\'t be zero')
     
 def check_odd_even(input_number):
-    if input_number % 2 == 0:    
-        'Entered number is even'
-    elif input_number %2 != 0:
-        'Entered number is odd'
-    else:
-        'You entered not a number.'
+    try:
+        valid_number(input_number)
+        return 'Entered number is even' if input_number % 2 == 0 else 'Entered number is odd'
+    except:
+        return'You entered not a number.'
