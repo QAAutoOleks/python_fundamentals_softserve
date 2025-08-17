639f90c84b410db9005a3a1d259434a3325f4081
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-17 16:26:00 +0300

    new file:   lesson_13_decorators.py/range_generator.py

---
+++ lesson_13_decorators.py/range_generator.py
@@ -0,0 +1,13 @@
+def range_generator(num):
+    x = 0
+    while x < num:
+        yield x
+        x += 1
+
+value = range_generator(5)
+print(value)
+print(next(value))
+print(next(value))
+print(next(value))
+print(next(value))
+print(next(value))
