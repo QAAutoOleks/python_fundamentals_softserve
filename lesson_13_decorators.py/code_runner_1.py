b1fff639d3a2f2e1e82c96b3fc54ae59b2df50cf
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-17 18:46:51 +0300

    new file:   lesson_13_decorators.py/code_runner_1.py

---
+++ lesson_13_decorators.py/code_runner_1.py
@@ -0,0 +1,35 @@
+from functools import reduce
+
+def fibonacci_numbers(num):
+    iter = 0
+    x1 = 0
+    x2 = 0
+    while iter < num:
+        if x2 >= 2:
+            yield x2
+            x1, x2 = x2, x1
+            x2 += x1
+        elif x1 == 1 and x2 == 1:
+            yield x2
+            x2 += x1
+        elif x1 == 1 and not x2:
+            yield 1
+            x2 += x1
+        elif x1 == 0:
+            yield 0
+            x1 += 1
+        iter += 1
+
+    
+fib = fibonacci_numbers(10)
+print(fib)
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
