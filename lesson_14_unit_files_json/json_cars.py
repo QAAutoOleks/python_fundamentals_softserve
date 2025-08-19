faff8c4b76501da9314ecaed1781eafcabcb305b
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-19 21:55:10 +0300

    modified:   cars.json modified:   lesson_14_unit_files_json/json_cars.py

--- lesson_14_unit_files_json/json_cars.py
+++ lesson_14_unit_files_json/json_cars.py
@@ -2,15 +2,23 @@ import json
 
 def sort_cars(dict):
     print('Before sorting:', dict)
-    for i in range(1, len(dict)):
-        if dict[i]['max_speed'] < dict[i-1]['max_speed']:
-            dict[i], dict[i-1] = dict[i-1], dict[i]
+    sorted = True
+    i = 0
+    while sorted:
+        sorted = False
+        try:
+            if dict[i]['max_speed'] > dict[i+1]['max_speed']:
+                dict[i], dict[i+1] = dict[i+1], dict[i]
+                sorted = True
+        except:
+            i = 0
+        i += 1
     return dict
 
 file_cars = open("cars.json", "r")
 cars_dict_1 = json.load(file_cars)
 
-print(sort_cars(cars_dict_1))
+print('After sorting', sort_cars(cars_dict_1))
 
 # collect_array = [car['max_speed'] for car in cars_dict_1]
 # file_cars2 = open("cars2.json", "r")
