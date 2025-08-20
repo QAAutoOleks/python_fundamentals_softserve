15b5ad9a28b1b32f13ee41914c30594a2292ed3c
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-20 19:08:49 +0300

    modified:   lesson_14_unit_files_json/json_cars.py new file:   result.json

--- lesson_14_unit_files_json/json_cars.py
+++ lesson_14_unit_files_json/json_cars.py
@@ -1,29 +1,13 @@
 import json
 
-def sort_cars(dict):
-    print('Before sorting:', dict)
-    sorted = True
-    i = 0
-    while sorted:
-        sorted = False
-        try:
-            if dict[i]['max_speed'] > dict[i+1]['max_speed']:
-                dict[i], dict[i+1] = dict[i+1], dict[i]
-                sorted = True
-        except:
-            i = 0
-        i += 1
-    return dict
-
 file_cars = open("cars.json", "r")
 cars_dict_1 = json.load(file_cars)
 
-print('After sorting', sort_cars(cars_dict_1))
+file_cars2 = open("cars2.json", "r")
+cars_dict_2 = json.load(file_cars2)
 
-# collect_array = [car['max_speed'] for car in cars_dict_1]
-# file_cars2 = open("cars2.json", "r")
-# cars_dict_2 = json.load(file_cars2)
-# collect_array.append(cars_dict_2[0]['max_speed'])
-# collect_array = sorted(collect_array)
-# result_file = open("result.json", "w")
-# result_file.write(collect_array)
+result_file = open("result.json", "w")
+
+merged_list = cars_dict_1 + cars_dict_2
+sorted_dict = sorted(merged_list, key=lambda speed: speed["max_speed"])
+result_file.write(str(sorted_dict))
