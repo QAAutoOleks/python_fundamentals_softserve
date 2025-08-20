f124e748cf242e597ae6d82177e9c2072bd08857
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-20 21:14:11 +0300

    modified:   lesson_14_unit_files_json/list_sort.py

--- lesson_14_unit_files_json/list_sort.py
+++ lesson_14_unit_files_json/list_sort.py
@@ -1,25 +1,15 @@
 some_list = [4, -6, 7, 100, 3]
 
 def sort_func(given_list):
-    iteration = 0
     sorted_list = []
     for number in given_list:
-        if not sorted_list:
-            sorted_list.append(number)
-        elif number < sorted_list[iteration-1]:
-            sorted_list.append(number)
-            (
-            sorted_list[iteration-1], 
-            sorted_list[iteration]
-            ) = (
-            sorted_list[iteration], 
-            sorted_list[iteration-1]
-            )
+        for i, val in enumerate(sorted_list):
+            if number < val:
+                sorted_list.insert(i, number)
+                break
         else:
             sorted_list.append(number)
-
-        iteration += 1
-    
     return sorted_list
+
 
 print(sort_func(some_list))
