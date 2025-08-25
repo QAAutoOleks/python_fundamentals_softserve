8e9ad82997b11e67dc9e071535e7791997e95b6c
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2025-08-25 19:45:16 +0300

    modified:   lesson_5_collections/practical_task_1.py

--- lesson_5_collections/practical_task_1.py
+++ lesson_5_collections/practical_task_1.py
@@ -1,22 +1,22 @@
+def enum_map(func):
+    def wrapper(given_str):      
+        created_list = func(given_str)
+
+        result = []
+        for j, a in enumerate(created_list):
+            result.append(j + a)
+
+        return result    
+
+    return wrapper
+
+@enum_map
 def compreh_list(input):
     return [int(i.strip()) for i in input.split(',')]
     
+@enum_map
 def map_list(input):
     return list(map(lambda x: int(x.strip()), input.split(',')))
 
-def enum_compreh(given_input):
-    result = []
-    for j, a in enumerate(compreh_list(given_input)):
-        result.append(j + a)
-
-    return result
-
-def enum_map(given_input):
-    result = []
-    for j, a in enumerate(map_list(given_input)):
-        result.append(j + a)
-
-    return result
-
-print(enum_compreh('1,  2, 3,  4'))
-print(enum_map('1, 2,  3,  4'))
+print(compreh_list('1,   2, 3'))
+print(map_list('1,   2, 3'))
